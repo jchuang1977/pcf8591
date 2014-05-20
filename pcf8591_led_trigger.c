025d97e1d42eea0fdd1f26ce2b5f2152ad597362
jchuang1977 <jchuang1977@gmail.com>
2014-05-21 00:08:01 +0800

    Add led trigger c

---
+++ pcf8591_led_trigger.c
@@ -0,0 +1,17 @@
+#include <wiringPi.h>
+#include <pcf8591.h>
+
+main ()
+{
+  wiringPiSetup () ;
+  pcf8591Setup (200, 0x48) ;
+
+  for (;;)
+  {
+   // printf ("%4d %4d %4d %4d\n", analogRead (200), analogRead (201), analogRead (202), analogRead (203)) ;
+    sleep(2);
+     //printf("%4d\n",analogRead(203));
+     analogWrite(200,analogRead(203));
+    
+  }
+}
